867f10d9d59ba3393e877e589f988f868c1c66f7
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2024-11-29 03:57:33 +0100

    grab the needed html elements

---
+++ script.js
@@ -0,0 +1,5 @@
+let formElem = document.querySelector('.form-container')
+let searchBoxElem = document.querySelector('.search-box')
+let searchButtonElem = document.querySelector('.search-btn')
+let searchResultElem = document.querySelector('.search-btn')
+
