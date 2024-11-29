320cc99b19376ae938bb1041873c8cbc61030286
Adeniji Aliu Adeyemi <adenijialiuadeyemi@gmail.com>
2024-11-29 04:09:56 +0100

    Grab data from unsplash API

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-let formElem = document.querySelector('.form-container')
+const formElem = document.querySelector('.form-container')
 let searchBoxElem = document.querySelector('.search-box')
-let searchButtonElem = document.querySelector('.search-btn')
+const searchButtonElem = document.querySelector('.search-btn')
 let searchResultElem = document.querySelector('.search-btn')
 
+let accessKey = '-iWLP2ImT8Bx5MtrgOhWe5R_Iqy7zhNLGYzgnTRmAdE'
+
+let page;
+let keyword = ''
+
+
+async function searchImages() {
+  keyword = searchBoxElem.value
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=${12}`
+
+  const response = await fetch(url)
+  const data = await response.json()
+
+  console.log(data)
+}
+
+formElem.addEventListener('submit', (e) => {
+  e.preventDefault()
+  page = 1
+  searchImages()
+})
